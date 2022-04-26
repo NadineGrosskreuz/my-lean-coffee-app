4199acefcf417708471b85c4f1d29f496c032982
Nadine Großkreuz <nadine.grosskreuz@web.de>
2022-04-26 14:34:10 +0200

    Add mui

--- pages/src/components/Header.js
+++ pages/src/components/Header.js
@@ -1,13 +1,30 @@
+import { AppBar, Button, Container, Toolbar } from "@mui/material";
 import Link from "next/link";
 
 export default function Header() {
   return (
-    <header>
-      <Link href="/">Home</Link>
-      <br />
-      <Link href="/cards">Cards</Link>
-      <br />
-      <Link href="/create">Create</Link>
-    </header>
+    <AppBar color="primary">
+      <Container maxWidth="xl">
+        <Toolbar>
+          <Link href="/" passHref>
+            <Button component="a" sx={{ color: "white" }}>
+              Home
+            </Button>
+          </Link>
+
+          <Link href="/cards" passHref>
+            <Button component="a" sx={{ color: "white" }}>
+              Cards
+            </Button>
+          </Link>
+
+          <Link href="/create" passHref>
+            <Button component="a" sx={{ color: "white" }}>
+              Create
+            </Button>
+          </Link>
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
 }
